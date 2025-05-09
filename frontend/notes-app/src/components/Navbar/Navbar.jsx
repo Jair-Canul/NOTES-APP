24d75148c27024feb64cb18c6c61dc1729235b5f
Jair Canul <jdcs23032001@gmail.com>
2025-05-08 21:50:41 -0600

    first commit

---
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Navbar = () => {
+    return (
+        <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+            <h2 className="text-xl font-medium text-black py-2">Notes </h2>
+
+        </div>
+    )
+}
+
+export default Navbar
+// //Este código es un componente funcional de React llamado "Navbar". Este componente simplemente devuelve un elemento <div> que contiene el texto "Navbar".   
---
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SignUp = () => {
+    return (
+        <div>SignUp</div>
+    )
+}
+
+export default SignUp
+// //Este código es un componente funcional de React llamado "SignUp". Este componente simplemente devuelve un elemento <div> que contiene el texto "SignUp".
